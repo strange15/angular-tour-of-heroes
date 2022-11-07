1137941b4ad28f236a64d85eda49dbb777678c28
Strange <strange66666666@gmail.com>
2022-11-07 18:18:55 +0800

    建立模擬（mock）的英雄資料

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../heroes/hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +8,18 @@ import { Hero } from '../heroes/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    const testString:string = 'test';
+    console.log("🚀 ~ file: heroes.component.ts ~ line 26 ~ HeroesComponent ~ onSelect ~ hero", hero)
+    this.selectedHero = hero;
+  }
 }
