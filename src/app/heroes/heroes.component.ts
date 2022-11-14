e3551f74b042a61ce72453640c7a0a086ba99865
Strange <strange66666666@gmail.com>
2022-11-14 12:37:21 +0800

    add Service; used Observable of RxJS

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../heroes/hero';
-import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add(`HeroesComponent:
+      ${ this.selectedHero === hero ? 'Unselected' : 'Selected' } hero id=${hero.id}`);
     this.selectedHero = this.selectedHero === hero ? undefined : hero;
   }
 }
